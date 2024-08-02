c8aebbef3dfb12f63cc045538fac33ad593dacd6
mhuzaifi0604 <huzzaifaasim@gmail.com>
2024-08-02 16:29:43 +0500

    Web scraper done

---
+++ src/Products.jsx
@@ -0,0 +1,14 @@
+const products = [
+    "headphones",
+    "smartphones",
+    "laptops",
+    "smartwatches",
+    "gaming keyboards",
+    "wireless chargers",
+    "bluetooth speakers",
+    "digital cameras",
+    "fitness trackers",
+    "tablet accessories"
+];
+
+export default products;
